ff75b528672d10de7a90136c7179d2269894a7f4
fevico <ajayifavour81@gmail.com>
2025-09-19 23:47:25 +0100

    added home diretory to header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 const Header = () => {
     return (
         <header className="bg-gray-800 text-white p-4">
-            <h1 className="text-2xl font-bold">Bible App</h1>
+            <h1 className="text-2xl font-bold"><Link href="/">Bible App</Link></h1>
         </header>
-    );
+    );      
 }
  
 export default Header;
